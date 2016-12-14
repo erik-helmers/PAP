3582a2e3fb27da088eff33e525bc9571ab458c2c
Keroths <keroths@mail.com>
2016-12-14 10:38:34 +0100

    Update main.py
    Implémentation de navigateur, de buffer.

--- main.py
+++ main.py
@@ -7,37 +7,50 @@ else:
     from tkinter import *
 
 
-_funcs_screen_ = ["toggle_fullscreen", "end_fullscreen", "leave", "background"]
+
+
+__funcs_screen = ["toggle_fullscreen", "end_fullscreen","switch_fullscreen", "leave", "background", "getScreen", "update", "c_empty", "c_undo"]
+__funcs_pen = ["cercle", "forward", "rotate", "left", "right", "undo", "empty", "setColor"]
 
 class PAP_Error(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        return repr(self.value)
+        return repr("Une erreur est survenue:",self.value)
+
+class Terminated(Exception):
+    def __init__(self): pass
+    def __str__(self):
+        return repr("\n\n\nLa fenêtre a été fermée. Les commandes sont désactivées.\n\n\n")
 
 def debug(s, t="ALL"):
     try:
         if DEBUG[t]or DEBUG["ALL"]:
             print(s)
-    except KeyError: print("Unknown type %s, passing debug" %(t))
+    except KeyError: print("Unknown type %s, passing debug" %(t)); print(s)
 
 
     
-class MWindow:
+class Screen:
 
-    def __init__(self):
-            
+    __name__ = "Screen"
+
+    def __init__(self, root=None):
+
+        
         #Parametres de FullScreen
         self.fullscreen = FULLSCREEN
+
+        if root==None:self.tk = Tk() #Permet l'importation de la fenetre en tant que frame
+        else: self.tk = root        #ou en tant que standalone.
         
-        self.tk = Tk() #Root
         self.frame = Frame(self.tk)
         self.frame.pack()
         
         #Binding 
         self.tk.bind("<F11>", self.toggle_fullscreen)
         self.tk.bind("<Escape>", self.end_fullscreen)
-        self.tk.bind("<Configure>", self.reconfig)
+        self.tk.bind("<Configure>", self.__reconfig__)
         
         self.tk.protocol("WM_DELETE_WINDOW", self.leave)
         
@@ -45,7 +58,10 @@ class MWindow:
         
         self.canvas = Canvas(self.tk, width = 800, height = 800, bg="white")
         self.canvas.pack(expand=True, fill='both')
-            
+
+    def reset(self):
+        self.canvas.delete(ALL)
+        self.update()
             
     def toggle_fullscreen(self, event=None):
         self.state = True  # Just toggling the boolean
@@ -66,17 +82,31 @@ class MWindow:
     def leave(self, event=None):
         if not CONFIRM_QUIT or messagebox.askyesno("Quitter", "Voulez-vous vraiment quitter ? #Tristesse"):
             self.tk.destroy()
+        raise Terminated
         
     def __reconfig__(self, event):
         x, y = event.width//2, event.height//2
         self.canvas.config(scrollregion=(-x, -y, x, y))
 
-    def background(color):
+    def background(self, color):
         if isinstance(color, (int, int, int)):
             pass
+        else: self.canvas.config(background=color)
         
-        
-class _Head_Navigator:
+    def getScreen(self): return self
+    
+    def update(self):
+        self.tk.grab_set()
+        self.tk.focus_set()
+        self.canvas.update()
+
+    def c_empty(self): return self.canvas.find_all()==()
+    def c_undo(self):
+        try:
+            self.canvas.delete(self.canvas.find_all()[-1])
+        except: pass
+        self.update()
+class __Head_Navigator:  #Implementation de la gestion des positions
 
     def __init__(self, pos=(0,0), dire = 0):
 
@@ -84,41 +114,127 @@ class _Head_Navigator:
         self.y = pos[1]
         self.dir = math.radians(dire)
 
+    def get(self):
+        return ((self.x, self.y), self.dir)
+    
     def forward(self, x):
 
         self.x = (self.x+(x*(math.cos(self.dir))))
         self.y = (self.y+(x*(math.sin(self.dir))))
         return (self.x, self.y)
 
-    def left(angle): self.dir -= math.radians(dire)
-    def right(angle): self.dir += math.radians(dire)
-    def rotate(angle): right(angle)
+    def left(self, angle): self.dir -= math.radians(angle)
+    def right(self, angle): self.dir += math.radians(angle)
+    def rotate(self, angle): self.right(angle)
+
+    def reset(self):
+        self.x = self.y = self.dir= 0
+
+ 
+#Classe gestion de buffering, peut etre améliorée
+        
+
+class __Item_Change_Buffer:
+
+    def __init__(self, size=2000): self.buff = []; self.size = size
+    def empty(self): return self.buff==[]
+    def pop(self): return self.buff.pop(-1)
+    def add(self, item):
+        if len(self.buff)==self.size: self.buff.pop(0)
+        self.buff.append(item)
 
 
-class Pen(_Head_Navigator):
+class Navigator(__Head_Navigator):
 
+    __obj_counter = 0
+    
+    def __init__(self, pos=(0,0), dire = 0, shown=False):
+
+        _HN.__init__(self, pos, dire)
+
+        Navigator.__obj_counter +=1
+        self.id = "Navigator_"+str(Navigator.__obj_counter)
+        
+        self.w = getScreen()
+        self.shown = shown
+
+        self.show()
+        
+    def link(self, other, couleur="black", epaisseur=LINE_WIDTH):
+        debug(*_HN.get(self)[0], *_HN.get(other)[0])
+        self.w.canvas.create_line(*_HN.get(self)[0], *_HN.get(other)[0],  width=epaisseur,
+                                  smooth=0, fill = couleur)
+        self.w.update()
+
+    def forward(self, x):
+        _HN.forward(self, x)
+        if self.shown: self.show()
+        
+    def show(self):
+        if self.shown :
+            self.w.canvas.delete(self.id)
+            Pen.cercle(self, 5)
+        self.w.update()
+
+class Pen(__Head_Navigator, __Item_Change_Buffer):
+
+    __name__ = "Pen"
+    __obj_count__ = 0
+    
     def __init__(self, pos = (0,0), direction = 0):
 
-        self.pos = pos
-        self.direction = direction
-
-        self.w = _Screen
+        
+        _HN.__init__(self, pos, direction)
+        _ICB.__init__(self)
+        
+        Pen.__obj_count__ += 1
+        self.id = "Pen_"+str(Pen.__obj_count__)
+         
+        self.w = getScreen()
+        self.color = "black"
         debug(("got it,", self.w))
+
+    def __setDefault__(self, kwargs, output, kw, trslt, dflt):
+        if kw in kwargs: output[trslt]=kwargs[kw]
+        else : output[trslt]=dflt
         
+    def __getOptions__(self, kwargs):
+        output = {}
+        return output
     
-    def cercle(self, r, couleur = "", contour = "black", epaisseur=LINE_WIDTH):
-        
-        debug((r, couleur, contour, epaisseur), "PEN")
+    def cercle(self, r, couleur="", contour="black", epaisseur=LINE_WIDTH):
+
+        debug((r, couleur, contour, epaisseur, self.id), "DRAWING")
+
         self.w.canvas.create_oval(self.x-r, self.y-r, self.x+r, self.y+r,
-                             fill=couleur, outline=contour, width=epaisseur)
+                             fill=couleur, outline=contour, width=epaisseur, tag=self.id)
+        self.w.update()
 
+    def forward(self, x, epaisseur=LINE_WIDTH):
+        
+        self.buff.append(
+            self.w.canvas.create_line(*_HN.get(self)[0], *_HN.forward(self, x),
+                                      tag = self.id, width=epaisseur, smooth=True, fill = self.color)
+            )
+        self.w.update()
 
+    def reset(self):
+        _HN.reset(self)
 
+    def undo(self):
+        self.w.canvas.delete(_ICB.pop(self))
+        self.w.update()
+        
+    def setColor(self, color): self.color = color
+    def setLineWidth(self, width): self.lineWidth = width
+
+    
+#Raccourci pour accès classe
+_HN = __Head_Navigator
+_ICB = __Item_Change_Buffer
+
+        
 ###Permet de faire forward(x) au lieu de p.forward(x)###
-
-
-_Screen = None
-_DfltPen = None
 
 
 #Creation d'une fonction qui sera ensuite adapté grace a un format().
@@ -128,17 +244,22 @@ _DfltPen = None
 
 func_format = """\
 def {name}({args}):
-    global a
+    global {obj}
     if {obj}==None: {obj} = {cls}()
     try:
         return {obj}.{name}({args_name})
-    except e: raise PAP_Error("Error", e)   
+    except: raise PAP_Error("Error", sys.exc_info())   
 """
-
+def some_iter(l):
+    for x in l:
+        if isinstance(x, str):
+            yield '"'+x+'"'
+        else: yield x
 def get_args_list(func):
 
     a, var, kwarg, b = inspect.getargspec(func)  #Output -> args
     if b:
+        b = [x for x in some_iter(b)]
         output = a[1:len(a)-len(b)]+list(map(lambda x:"%s=%s" %(x[0], x[1]),  #Cette ligne a pour but d'ajouter les premiers argument sans valeur par defaut, et d'assembler 
                                  (zip(a[len(a)-len(b)::1], b))))              #Les arguments avec des valeurs par defaut. Le zip fait des tuple de a[n], b[n]. La lambda le depack et en fait un "{argname}={defaultvalue}" 
     else: output = a[1::1]
@@ -149,15 +270,20 @@ def get_args_list(func):
         temp="**%s" %(kwarg);output.append(temp);default.append(temp)
     return output, default
 
-def construct_func(funcs, obj_name):
-    exec("obj = %s"%(obj_name), globals())
-    clss = obj.__class__
+def construct_func(funcs, clss, varName = None): #La classe entrée doit posséder un attribut __name__
+    if varName == None: varName = "_"+clss.__name__
+    debug(varName)
+    exec("%s = None"%(varName), globals())
     for funcname in funcs:
         func = getattr(clss, funcname)
         args_entry, args = get_args_list(func)
         exec(func_format.format(name=funcname, args = ",".join(args_entry),
-                                args_name = ",".join(args), cls=clss.__name__, obj=obj_name),
+                                args_name = ",".join(args), cls=clss.__name__, obj=varName),
              globals())
 
+construct_func(__funcs_screen, Screen)
+construct_func(__funcs_pen, Pen)
 
-    
+def reset():
+    _Screen.reset()
+    _Pen.reset()
